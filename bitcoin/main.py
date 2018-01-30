e780a9e2eb4ec9537e8ba9fa0079f533663094b3
xwren <seektolive@gmail.com>
2018-01-30 11:21:10 +0800

    test

--- bitcoin/main.py
+++ bitcoin/main.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Author: xwren
-# @Date:   2018-01-30 10:52:52
-# @Last Modified by:   xwren
-# @Last Modified time: 2018-01-30 10:52:52
+
+if __name__ == '__main__':
+	main()
